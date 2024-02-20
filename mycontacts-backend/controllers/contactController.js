fafeb9a1cd495df2d099415a7bedf233ddf8d4f2
furkanyilmazPetroo <furkan.yilmaz@anttechbva.com>
2024-02-20 14:36:05 +0300

    Backend serverinin kurulumunun tamamlanması

--- mycontacts-backend/controllers/contactController.js
+++ mycontacts-backend/controllers/contactController.js
@@ -1,39 +1,47 @@
+const asyncHandler = require("express-async-handler");
+
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
-const getAllContacts = (req, res) => {
+const getAllContacts = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get all contacts" });
-};
+});
 
 //@desc Create new contacts
 //@route POST /api/contacts
 //@access public
-const createContact = (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("All fields are mndatory!");
+  }
+  console.log(name, email, phone);
   res.status(201).json({ message: "Create new contact" });
-};
+});
 
 //@desc Get specified contact
 //@route GET /api/contacts
 //@access public
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
   res
-    .status(404)
+    .status(200)
     .json({ message: `Get the contact with id: ${req.params.id}` });
-};
+});
 
 //@desc Update specified contact
 //@route PUT /api/contacts
 //@access public
-const updateContact = (req, res) => {
-  res.status(404).json({ message: `Update contact with id: ${req.params.id}` });
-};
+const updateContact = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Update contact with id: ${req.params.id}` });
+});
 
 //@desc Delete specified contact
 //@route DELETE /api/contacts
 //@access public
-const deleteContact = (req, res) => {
-  res.status(404).json({ message: `Delete contact with id: ${req.params.id}` });
-};
+const deleteContact = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Delete contact with id: ${req.params.id}` });
+});
 
 module.exports = {
   getAllContacts,
